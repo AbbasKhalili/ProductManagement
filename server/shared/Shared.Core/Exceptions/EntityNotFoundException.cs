304be3fceac6aea596c61cbcd29d317a46fce695
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-07 04:32:41 +0430

    added BusinessException class and some general exception in shared lib

---
+++ server/shared/Shared.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Core.Exceptions
+{
+    public class EntityNotFoundException : BusinessException
+    {
+        public EntityNotFoundException() : base(1, "Notfound!")
+        {
+        }
+        public EntityNotFoundException(long id) : base(1, $"Id: {id} NotFound!")
+        {
+        }
+        public EntityNotFoundException(string message) : base(1, message)
+        {
+        }
+    }
+}
