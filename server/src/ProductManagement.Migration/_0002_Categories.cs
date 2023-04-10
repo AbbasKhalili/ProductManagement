5c8157afa02281fe6d0f9b66ac2858227a3c2d07
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-10 11:50:56 +0430

    added Category table by migration

---
+++ server/src/ProductManagement.Migration/_0002_Categories.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductManagement.Migration
+{
+    [Migration(2)]
+    public class _0002_Categories : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Categories")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("SurrogateKey").AsGuid()
+                .WithColumn("Name").AsString()
+                .WithColumn("CreatedDate").AsDateTime2()
+                .WithColumn("LastModified").AsDateTime2()
+                .WithColumn("IsDeleted").AsBoolean()
+                .WithColumn("DeletedDate").AsDateTime2();
+        }
+    }
+}
