1678b5f0caac711ae582ffa47af37ada35a77dc4
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-10 11:47:59 +0430

    added Products table by migration

---
+++ server/src/ProductManagement.Migration/_0001_Products.cs
@@ -0,0 +1,24 @@
+﻿global using FluentMigrator;
+
+namespace ProductManagement.Migration
+{
+    [Migration(1)]
+    public class _0001_Products : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Products")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("SurrogateKey").AsGuid()
+                .WithColumn("Name").AsString()
+                .WithColumn("Weight").AsDouble()
+                .WithColumn("ProductType").AsByte()
+                .WithColumn("Description").AsString()
+                .WithColumn("CategoryId").AsInt64()
+                .WithColumn("CreatedDate").AsDateTime2()
+                .WithColumn("LastModified").AsDateTime2()
+                .WithColumn("IsDeleted").AsBoolean()
+                .WithColumn("DeletedDate").AsDateTime2();
+        }
+    }
+}
