382bef3715e849814e4353ea6209f9bf7f68c107
Abbas Khalili <khalilikhoo2@hotmail.com>
2023-04-11 18:34:26 +0330

    added Query side Service

---
+++ server/src/ProductManagement.Interface.Query/ProductQueryService.cs
@@ -0,0 +1,18 @@
+﻿using ProductManagement.Interface.Contract.Product.Dtos;
+using ProductManagement.Interface.Contract.Product.Services;
+
+namespace ProductManagement.Interface.Query
+{
+    public class ProductQueryService : IProductQueryService
+    {
+        public async Task<List<ProductDto>> GetAll()
+        {
+            
+        }
+
+        public async Task<ProductDto> GetById(Guid id)
+        {
+
+        }
+    }
+}
