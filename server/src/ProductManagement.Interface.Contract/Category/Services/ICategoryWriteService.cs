a588a5747c6fc37e3b3e2f4f85e3bc8063ddb3d9
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-12 16:12:57 +0330

    implement write model for Category

--- server/src/ProductManagement.Interface.Contract/Category/Services/ICategoryWriteService.cs
+++ server/src/ProductManagement.Interface.Contract/Category/Services/ICategoryWriteService.cs
@@ -5,7 +5,7 @@ namespace ProductManagement.Interface.Contract.Category.Services
     public interface ICategoryWriteService
     {
         Task<Guid> Create(CategoryModel model);
-        Task<Guid> Update(Guid id, CategoryModel model);
-        Task<Guid> Delete(Guid id);
+        Task Update(Guid id, CategoryModel model);
+        Task Delete(Guid id);
     }
 }
---
+++ server/src/ProductManagement.Interface.Write/CategoryWriteService.cs
@@ -0,0 +1,46 @@
+﻿using ProductManagement.Domain.Categories;
+using ProductManagement.Interface.Contract.Category.Models;
+using ProductManagement.Interface.Contract.Category.Services;
+using Shared.Core;
+
+namespace ProductManagement.Interface.Write
+{
+    internal class CategoryWriteService : ICategoryWriteService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly ISystemClock _systemClock;
+
+        public CategoryWriteService(ICategoryRepository categoryRepository, ISystemClock systemClock)
+        {
+            _categoryRepository = categoryRepository;
+            _systemClock = systemClock;
+        }
+
+        public async Task<Guid> Create(CategoryModel model)
+        {
+            var category = new Category(model.Name, _systemClock);
+
+            await _categoryRepository.Create(category);
+
+            return category.SurrogateKey;
+        }
+
+        public async Task Update(Guid id, CategoryModel model)
+        {
+            var category = await _categoryRepository.GetBy(id);
+
+            category.Update(model.Name, _systemClock);
+
+            await _categoryRepository.Update(category);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var category = await _categoryRepository.GetBy(id);
+
+            category.Delete(_systemClock);
+
+            await _categoryRepository.Delete(category);
+        }
+    }
+}
