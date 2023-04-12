0a4fa793778ab9e105713d027c23ec121e549719
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-13 02:20:14 +0430

    create write/read model api(controller)

---
+++ server/src/ProductManagementRestAPI/Controllers/ProductQueryController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Interface.Contract.Product.Dtos;
+using ProductManagement.Interface.Contract.Product.Services;
+
+namespace ProductManagementRestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    public class ProductQueryController : ControllerBase
+    {
+        private readonly IProductQueryService _productQueryService;
+
+        public ProductQueryController(IProductQueryService productQueryService)
+        {
+            _productQueryService = productQueryService;
+        }
+
+        [HttpGet("getbyid/{id}")]
+        public async Task<ProductDto> GetById(Guid id)
+        {
+            return await _productQueryService.GetById(id);
+        }
+
+        [HttpGet("getall")]
+        public async Task<List<ProductDto>> GetAll()
+        {
+            return await _productQueryService.GetAll();
+        }
+    }
+}
---
+++ server/src/ProductManagementRestAPI/Controllers/ProductWriteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Interface.Contract.Product.Models;
+using ProductManagement.Interface.Contract.Product.Services;
+
+namespace ProductManagementRestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    public class ProductWriteController : ControllerBase
+    {
+        private readonly IProductWriteService _productWriteService;
+
+        public ProductWriteController(IProductWriteService productWriteService)
+        {
+            _productWriteService = productWriteService;
+        }
+
+        [HttpPost]
+        public async Task<Guid> Create(ProductModel model)
+        {
+            return await _productWriteService.Create(model);
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task Update(Guid id, ProductModel model)
+        {
+            await _productWriteService.Update(id,model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id)
+        {
+            await _productWriteService.Delete(id);
+        }
+    }
+}
