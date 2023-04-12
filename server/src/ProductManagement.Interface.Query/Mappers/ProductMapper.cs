edb0450e8ba1702dc7fe60e9dfe2e532c24c8ee6
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-13 02:21:57 +0430

    complete and implete readmodel

---
+++ server/src/ProductManagement.Interface.Query/Mappers/ProductMapper.cs
@@ -0,0 +1,29 @@
+﻿using ProductManagement.Domain.Products;
+using ProductManagement.Interface.Contract.Product.Dtos;
+
+namespace ProductManagement.Interface.Query.Mappers
+{
+    internal static class ProductMapper
+    {
+        internal static List<ProductDto> Map(List<Product> list)
+        {
+            return list.Select(Map).ToList();
+        }
+
+        internal static ProductDto Map(Product item)
+        {
+            return new ProductDto
+            {
+                Name = item.Name,
+                Id = item.SurrogateKey,
+                Description = item.Description,
+                Enabled = item.Enabled,
+                Weight = item.Weight,
+                ProductType = (byte)item.ProductType,
+                ProductTypeTitle = item.ProductType.ToString(),
+                CategoryName = item.Category.Title,
+                CategoryId = item.Category.SurrogateKey,
+            };
+        }
+    }
+}
--- server/src/ProductManagement.Interface.Query/ProductQueryService.cs
+++ server/src/ProductManagement.Interface.Query/ProductQueryService.cs
@@ -1,18 +1,29 @@
-﻿using ProductManagement.Interface.Contract.Product.Dtos;
+﻿using ProductManagement.Domain.Products;
+using ProductManagement.Interface.Contract.Product.Dtos;
 using ProductManagement.Interface.Contract.Product.Services;
+using ProductManagement.Interface.Query.Mappers;
 
 namespace ProductManagement.Interface.Query
 {
     public class ProductQueryService : IProductQueryService
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductQueryService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public async Task<List<ProductDto>> GetAll()
         {
-            
+            var products = await _productRepository.GetAll();
+            return ProductMapper.Map(products);
         }
 
         public async Task<ProductDto> GetById(Guid id)
         {
-
+            var product = await _productRepository.GetBy(id);
+            return ProductMapper.Map(product);
         }
     }
 }
