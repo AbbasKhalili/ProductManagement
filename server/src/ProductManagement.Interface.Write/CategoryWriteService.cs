e3fcb85abb606c45c0eb156bf054840e5a248baa
abbas.khalili <khalilikhoo2@gmail.com>
2023-04-13 15:50:30 +0330

    injected IValidator into CategoryService writeModel

--- server/src/ProductManagement.Interface.Write/CategoryWriteService.cs
+++ server/src/ProductManagement.Interface.Write/CategoryWriteService.cs
@@ -1,4 +1,5 @@
-﻿using ProductManagement.Domain.Categories;
+﻿using FluentValidation;
+using ProductManagement.Domain.Categories;
 using ProductManagement.Interface.Contract.Category.Models;
 using ProductManagement.Interface.Contract.Category.Services;
 using Shared.Core;
@@ -9,16 +10,18 @@ namespace ProductManagement.Interface.Write
     {
         private readonly ICategoryRepository _categoryRepository;
         private readonly ISystemClock _systemClock;
+        private readonly IValidator<Category> _validator;
 
-        public CategoryWriteService(ICategoryRepository categoryRepository, ISystemClock systemClock)
+        public CategoryWriteService(ICategoryRepository categoryRepository, ISystemClock systemClock, IValidator<Category> validator)
         {
             _categoryRepository = categoryRepository;
             _systemClock = systemClock;
+            _validator = validator;
         }
 
         public async Task<Guid> Create(CategoryModel model)
         {
-            var category = new Category(model.Name, _systemClock);
+            var category = new Category(model.Name, _systemClock, _validator);
 
             await _categoryRepository.Create(category);
 
@@ -29,7 +32,7 @@ namespace ProductManagement.Interface.Write
         {
             var category = await _categoryRepository.GetBy(id);
 
-            category.Update(model.Name, _systemClock);
+            category.Update(model.Name, _systemClock, _validator);
 
             await _categoryRepository.Update(category);
         }
